0c7b4f08acfc313c17cdb036d815c79db64f4f39
DSlundro <dragan.savic95@outlook.it>
2022-04-07 14:56:02 +0200

    milestone 1

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,6 +10,62 @@
 //      organizzare i singoli membri in card/schede
 
 
+// creo un array con le informazioni del team
+const team = [
+    {
+    name: 'Wayne Barnett',
+    role: 'Founder & CEO',
+    image: 'wayne-barnett-founder-ceo.jpg',
+    },
+    {
+    name: 'Angela Caroll',
+    role: 'Chief Editor',
+    image: 'angela-caroll-chief-editor.jpg',
+    },
+    {
+    name: 'Walter Gordon',
+    role: 'Office Manager',
+    image: 'walter-gordon-office-manager.jpg',
+    },
+    {
+    name: 'Angela Lopez',
+    role: 'Social Media Manager',
+    image: 'angela-lopez-social-media-manager.jpg',
+    },
+    {
+    name: 'Scott Estrada',
+    role: 'Developer',
+    image: 'scott-estrada-developer.jpg',
+    },
+    {
+    name: 'Barbara Ramos',
+    role: 'Graphic Designer',
+    image: 'barbara-ramos-graphic-designer.jpg',
+    },
+];
+// stampo in console l'intero array
+console.log(team);
+
+// creo un ciclo per tirare fuori le informazioni di ogni elemento del array
+for (let i = 0; i < team.length; i++){
+    // creao una variabile per la singola persona
+    let person = team[i];
+    // stampo in console ogni singola informazione degli oggetti nel array
+    console.log(person)
+    console.log(`Name:      ${person.name}`);
+    console.log(`Role:      ${person.role}`);
+    console.log(`Image:     ${person.image}`);
+    
+    document.getElementById('team').innerHTML = person.name
+    
+}
+
+
+
+
+
+
+
 
 
 
